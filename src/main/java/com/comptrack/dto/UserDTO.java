fbe04be8e26140f5fbef09d59adcf6ec93342518
arc7kai <rakeshmdb589@gmail.com>
2025-06-04 11:57:30 +0530

    Added password field to UserDTO

--- src/main/java/com/comptrack/dto/UserDTO.java
+++ src/main/java/com/comptrack/dto/UserDTO.java
@@ -1,22 +1,12 @@
 package com.comptrack.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-
 public class UserDTO {
 
     private Long id;
-
-    @NotBlank(message = "Username is required")
-    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     private String username;
-
-    @NotBlank(message = "Email is required")
-    @Email(message = "Email must be valid")
     private String email;
+    private String password;
 
-    // Getters and Setters
     public Long getId() {
         return id;
     }
@@ -39,5 +29,13 @@ public class UserDTO {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
